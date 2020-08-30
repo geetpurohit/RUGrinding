7aa117c0a04917df561de62396c44302f0566471
geetpurohit <geetp40@gmail.com>
2020-08-30 02:19:54 -0400

    frontend

--- utils/messages.js
+++ utils/messages.js
@@ -4,7 +4,7 @@ function formatMessage(username, text){
     return {
         username,
         text,
-        time: moment().format('h:mm a')
+        time: moment().tz('America/New_York').format('h:mm a')
     }
 }
 
